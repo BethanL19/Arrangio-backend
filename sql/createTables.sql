9c11c271a397ea3925abbb8626e6dc9fa5419360
Beth <bethan.hp@gmail.com>
2023-08-29 11:33:03 +0100

    sql added & .env created

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,8 +1,34 @@
-DROP TABLE IF EXISTS food;
-
-CREATE TABLE  food (
-    id          serial PRIMARY KEY,
-    title       varchar(40) NOT NULL
+create table if not exists boards(
+board_id serial primary key,
+  name varchar(50)
 );
 
-INSERT INTO food (title) VALUES ('sourdough'), ('olive oil'), ('pizza'), ('picanha'), ('gorgonzola'), ('basil');
+insert into boards (name) values ('test board');
+select * from boards;
+
+
+create table if not exists lists(
+list_id serial primary key,
+  board_id int, FOREIGN KEY (board_id) REFERENCES boards(board_id),
+  name varchar(50)
+);
+insert into lists (board_id, name) values (1, 'To-Do'),(1, 'Doing'),(1, 'Done');
+select * from lists;
+
+
+create table if not exists cards(
+card_id serial primary key,
+  list_id int, FOREIGN KEY (list_id) REFERENCES lists(list_id),
+  name varchar(50)
+);
+insert into cards (list_id, name) values (1, 'Test'),(2, 'Test'),(3, 'Test');
+select * from cards;
+
+create table if not exists comments(
+comment_id serial primary key,
+  card_id int, FOREIGN KEY (card_id) REFERENCES cards(card_id),
+  text varchar(550)
+);
+insert into comments (card_id, text) values (1, 'Test comment');
+select * from comments;
+
